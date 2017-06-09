bbac2855b96c86895a4516942cba1e508bd7cf0e
Paulo de Olivera <paulo-gyn@Outlook.com>
2017-06-09 11:06:27 +0800

    Call using command line is now working

--- RepairPlugin/src/repairplugin/ParametersWizard.java
+++ RepairPlugin/src/repairplugin/ParametersWizard.java
@@ -34,47 +34,52 @@ public class ParametersWizard extends Wizard {
 	    @Override
 	    public boolean performFinish() {
 	        // Print the result to the console
-	    	Runtime rt = Runtime.getRuntime();
-//	    	try {
-	    		String codePath = one.getCodePath()+" ";
-	    		String fileName = one.getFileName()+" ";
-	    		String numberOfTests = two.getNumberOfTests() + " ";
-	    		String assertPath = two.getAssertJsonPath();
+	    	
+	    	String codePath = one.getCodePath()+" ";
+	    	String fileName = one.getFileName()+" ";
+	    	String numberOfTests = two.getNumberOfTests() + " ";
+	    	String assertPath = two.getAssertJsonPath();
 				
-//	    		final Process p = Runtime.getRuntime().exec(ANGELIX + codePath + fileName
-//	    				+ ORACLE + numberOfTests + ASSERT + assertPath);
-//
-//	    		new Thread(new Runnable() {
-//	    		    public void run() {
-//	    		     BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
-//	    		     String line = null; 
-//
-//	    		     try {
-//	    		        while ((line = input.readLine()) != null)
-//	    		            System.out.println(line);
-//	    		     } catch (IOException e) {
-//	    		            e.printStackTrace();
-//	    		     }
-//	    		    }
-//	    		}).start();
-//
-//	    		p.waitFor();
+//	    	final Process p = Runtime.getRuntime().exec(ANGELIX + codePath + fileName
+//	    			+ ORACLE + numberOfTests + ASSERT + assertPath);
 	    		
-	    		System.out.println(ANGELIX + codePath + fileName
-	    				+ ORACLE + numberOfTests + ASSERT + assertPath);
-	    		
-//			} catch (IOException e) {
-//				// TODO Auto-generated catch block
-//				e.printStackTrace();
-//			} catch (InterruptedException e) {
-//				// TODO Auto-generated catch block
-//				e.printStackTrace();
-//			}
-	    	
-	    	
+	    	String[] cmd = {
+			        "/bin/bash",
+			        "-c",
+			        "cd /home/angelix; python Shell.py"
+			    };
 
+			System.out.println(executeCommand(cmd));
+	    		
+	    	System.out.println(ANGELIX + codePath + fileName
+	    				+ ORACLE + numberOfTests + ASSERT + assertPath);
+	    	
 	        return true;
 	    }
+	    
+	    private String executeCommand(String[] command) {
+
+			StringBuffer output = new StringBuffer();
+
+			Process p;
+			try {
+				p = Runtime.getRuntime().exec(command);
+				p.waitFor();
+				BufferedReader reader =
+	                            new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+	                        String line = "";
+				while ((line = reader.readLine())!= null) {
+					output.append(line + "\n");
+				}
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+			return output.toString();
+
+		}
 	
 	
 	
--- RepairPlugin/src/repairplugin/handlers/SampleHandler.java
+++ RepairPlugin/src/repairplugin/handlers/SampleHandler.java
@@ -1,5 +1,9 @@
 package repairplugin.handlers;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
@@ -21,8 +25,43 @@ public class SampleHandler extends AbstractHandler {
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
+		String[] cmd = {
+		        "/bin/bash",
+		        "-c",
+		        "source /home/angelix/angelix/activate;"
+		        + "cd /home/angelix/angelix/src/tools;"
+		        + "./angelix -h"
+		    };
+
+		System.out.println(executeCommand(cmd));
+		
 		WizardDialog dialog = new WizardDialog(window.getShell(), new ParametersWizard());
 		dialog.open();
+		
 		return null;
+	}
+	
+	private String executeCommand(String[] command) {
+
+		StringBuilder output = new StringBuilder().append("");
+
+		Process p;
+		try {
+			p = Runtime.getRuntime().exec(command);
+			p.waitFor();
+			BufferedReader reader =
+                            new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+                        String line = "";
+			while ((line = reader.readLine())!= null) {
+				output.append(line + "\n");
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return output.toString();
+
 	}
 }
